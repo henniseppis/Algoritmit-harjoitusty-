6c348f4168e0ea69d6c6e44f5d3508008d70c8bd
Henni Seppänen <104835482+henniseppis@users.noreply.github.com>
2024-04-08 12:45:32 +0300

    tests for class Board

--- src/tests/board_test.py
+++ src/tests/board_test.py
@@ -1,30 +1,98 @@
-#import unittest
-#import string 
-#from board.board import Board
-#
-#
-#class BoardStubb:
-#    def create_board_test(self):
-#        self.board = [[" " for _ in range(20)] for _ in range(20)]
-#    
-#    def print_board_test(self, board):
-#        """Prints the board to command line"""
-#        alpha = "  " + "   ".join(string.ascii_uppercase[:20])
-#        print(alpha)
-#
-#        for i, row in enumerate(board):
-#            print(f"{i+1}", end=" ")
-#            print(" | ".join(row))
-#            print(" " * 3 + "-" * (4 * len(board)))
-#
-#
-#class TestBoard(unittest.TestCase):
-#    def setUp(self):
-#        self.board = BoardStubb()
-#        self.board_functions = Board(size=20)
-#        
-#    def test_next_move(self):
-#        board = self.board.create_board_test()
-#        self.board_functions.next_move("X", 0, 6)
-#        self.assertEqual(board[0][6], "X")
-#    
+import unittest
+import string 
+from board.board import Board
+from UI.ui import UI
+
+
+class TestBoard(unittest.TestCase):
+    def setUp(self):
+        self.board = Board(size=20)
+        self.ui = UI()
+        
+    def test_next_move(self):
+        board = self.board.create_board()
+        self.board.next_move("X", 0, 6)
+        self.assertEqual(board[0][6], "X")
+
+    def test_win_check_diagonal(self):
+        board = self.board.create_board()
+        self.board.next_move("X", 0, 6)
+        self.board.next_move("X", 1, 5)
+        self.board.next_move("X", 2, 4)
+        self.board.next_move("X", 3, 3)
+        self.board.next_move("X", 4, 2)
+        win = self.board.check_win(board, 4,2,"X")
+        self.assertEqual(win , True)
+
+    def test_win_check_diagonal_false(self):
+        board = self.board.create_board()
+        self.board.next_move("X", 0, 6)
+        self.board.next_move("X", 1, 5)
+        self.board.next_move("X", 2, 4)
+        self.board.next_move("=", 3, 3)
+        self.board.next_move("X", 4, 2)
+        win = self.board.check_win(board, 4,2,"X")
+        self.assertEqual(win , False)
+
+    def test_win_check__horizontal(self):
+        board = self.board.create_board()
+        self.board.next_move("X", 2, 10)
+        self.board.next_move("X", 2, 11)
+        self.board.next_move("X", 2, 12)
+        self.board.next_move("X", 2, 13)
+        self.board.next_move("X", 2, 14)
+        win = self.board.check_win(board, 2,14,"X")
+        self.assertEqual(win , True)
+
+
+    def test_win_check__horizontal_false(self):
+        board = self.board.create_board()
+        self.board.next_move("X", 2, 10)
+        self.board.next_move("X", 2, 11)
+        self.board.next_move("X", 2, 12)
+        self.board.next_move("O", 2, 13)
+        self.board.next_move("X", 2, 14)
+        win = self.board.check_win(board, 2,14,"X")
+        self.assertEqual(win , False)
+    
+    def test_win_check__vertical(self):
+        board = self.board.create_board()
+        self.board.next_move("X", 3, 10)
+        self.board.next_move("X", 4, 10)
+        self.board.next_move("X", 5, 10)
+        self.board.next_move("X", 6, 10)
+        self.board.next_move("X", 7, 10)
+        win = self.board.check_win(board, 7,10,"X")
+        self.assertEqual(win , True)
+
+
+    def test_win_check__vertical_false(self):
+        board = self.board.create_board()
+        self.board.next_move("X", 3, 10)
+        self.board.next_move("X", 4, 10)
+        self.board.next_move("X", 5, 10)
+        self.board.next_move("O", 6, 10)
+        self.board.next_move("X", 7, 10)
+        win = self.board.check_win(board, 7,10,"X")
+        self.assertEqual(win , False)
+    
+    def test_check_draw(self):
+        board = self.board.create_board()
+        for i in range(0,20):
+            for x in range(0,20):
+                board[i][x] = "X"
+        draw = self.board.check_draw(board)
+        self.assertEqual(draw , True)
+    
+    def test_check_draw_false(self):
+        board = self.board.create_board()
+        for i in range(0,18):
+            for x in range(0,20):
+                board[i][x] = "X"
+        draw = self.board.check_draw(board)
+        self.assertEqual(draw , False)
+
+
+
+
+    
